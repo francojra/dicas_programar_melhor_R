5025f1af5318dc716d6bf0eac907ac4ffc923571
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-04-11 22:33:46 -0300

    Add files via upload

--- dicas_programas_melhor_R.R
+++ dicas_programas_melhor_R.R
@@ -5,3 +5,13 @@
 # Referência: https://www.youtube.com/watch?v=1YPXSh09VOU ----------------------------------------------------------------------------------
 
 
+# 1 - Organização --------------------------------------------------------------------------------------------------------------------------
+
+### Um script organizado permite menor gasto de tempo, como na busca por arquivos em
+### uma pasta, encontro de erros nos códigos, etc. Após um tempo sem utilizar o código
+### se torna difícil entrendê-lo, caso ele não esteja organizado. Interfere no comparti-
+### lhamento de arquivos com colegas.
+
+### Defina um modelo padrão de organização dos seus scripts para todos os projetos.
+
+### Crie projetos no RStudio para facilitar a organização.
