32b98e0b241ecca22ce6086f60a8647ec458c7b6
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-04-11 22:46:24 -0300

    Add files via upload

--- dicas_programas_melhor_R.R
+++ dicas_programas_melhor_R.R
@@ -16,5 +16,8 @@
 
 ### Crie projetos no RStudio para facilitar a organização. Interessante definir uma pasta
 ### para os scripts, e onde cada script é responsável por diferentes tipos de análises.
+### Pode-se definir uma pasta separada para os dados das análises, outra para apresentações,
+### outra para resultados, etc.
 
-
+### Um script do R pode ser feito com rascunhos das análises e ficar na mesma pasta dos
+### dados brutos.
